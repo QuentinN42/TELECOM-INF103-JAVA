c2d883f66f3b37c789e20b064ffa08d6469b0406
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-21 16:57:28 +0200

    feature(Vertex): Vertex class implemented

--- src/implement/graph/Vertex.java
+++ src/implement/graph/Vertex.java
@@ -4,4 +4,23 @@ import interfaces.graph.VertexInterface;
 
 public class Vertex implements VertexInterface
 {
+    private final int id;
+
+    public Vertex(int id)
+    {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object element)
+    {
+        if (element instanceof Vertex)
+        {
+            return this.id == ((Vertex) element).id;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
