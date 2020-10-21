a997735078cde256836aed881c60664d71b21362
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-21 17:21:36 +0200

    feature(Graph): Graph class implemented

--- src/implement/graph/Graph.java
+++ src/implement/graph/Graph.java
@@ -3,38 +3,55 @@ package implement.graph;
 import interfaces.graph.GraphInterface;
 import interfaces.graph.VertexInterface;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Graph implements GraphInterface
 {
+    private ArrayList<VertexInterface> vertexes = new ArrayList<>();
+    private int[][] weights;
+
+    public Graph(ArrayList<VertexInterface> vertexes, int[][] weights)
+    {
+        this.vertexes = vertexes;
+        this.weights = weights;
+    }
+
     @Override
     public boolean contains(VertexInterface element)
     {
-        return false;
+        return vertexes.contains(element);
     }
 
     @Override
     public boolean isSuccessor(VertexInterface father, VertexInterface child)
     {
-        return false;
+        return this.weight(father, child) != Integer.MAX_VALUE;
     }
 
     @Override
     public int weight(VertexInterface one, VertexInterface two)
     {
-        return 0;
+        if (this.contains(one) && this.contains(two))
+        {
+            return weights[this.vertexes.indexOf(one)][this.vertexes.indexOf(two)];
+        }
+        else
+        {
+            return Integer.MAX_VALUE;
+        }
     }
 
     @Override
     public int size()
     {
-        return 0;
+        return this.vertexes.size();
     }
 
     @Override
     public Iterator<VertexInterface> iterator()
     {
-        return null;
+        return this.vertexes.iterator();
     }
 
     @Override
