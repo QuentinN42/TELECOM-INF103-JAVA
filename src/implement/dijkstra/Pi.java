bc3f7a8995298e9cca1768be772b1bc5518313bd
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-21 14:54:11 +0200

    feature(Pi): Pi class implemented
    Derivated of Hashmap

--- src/implement/dijkstra/Pi.java
+++ src/implement/dijkstra/Pi.java
@@ -3,17 +3,28 @@ package implement.dijkstra;
 import interfaces.dijkstra.PiInterface;
 import interfaces.graph.VertexInterface;
 
+import java.util.HashMap;
+
 public class Pi implements PiInterface
 {
+    private final HashMap<VertexInterface, Integer> vertexes = new HashMap<>();
+
     @Override
     public void setValueOf(VertexInterface object, int value)
     {
-
+        vertexes.put(object, value);
     }
 
     @Override
     public int getValueOf(VertexInterface object)
     {
-        return 0;
+        if (this.vertexes.containsKey(object))
+        {
+            return vertexes.get(object);
+        }
+        else
+        {
+            return Integer.MAX_VALUE;
+        }
     }
 }
