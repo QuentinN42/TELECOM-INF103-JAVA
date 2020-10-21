a8903bf64d9a42c039575099c1a05166dfac48f7
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-21 15:24:24 +0200

    feature(ASet): ASet class implemented

--- src/implement/graph/ASet.java
+++ src/implement/graph/ASet.java
@@ -3,31 +3,41 @@ package implement.graph;
 import interfaces.graph.ASetInterface;
 import interfaces.graph.VertexInterface;
 
+import java.util.HashSet;
 import java.util.Iterator;
 
 public class ASet implements ASetInterface
 {
+    private final HashSet<VertexInterface> vertexes = new HashSet<>();
+
     @Override
     public boolean contains(Object element)
     {
-        return false;
+        if (element instanceof VertexInterface)
+        {
+            return vertexes.contains(element);
+        }
+        else
+        {
+            return false;
+        }
     }
 
     @Override
     public void add(VertexInterface element)
     {
-
+        vertexes.add(element);
     }
 
     @Override
     public int size()
     {
-        return 0;
+        return vertexes.size();
     }
 
     @Override
     public Iterator<VertexInterface> iterator()
     {
-        return null;
+        return vertexes.iterator();
     }
 }
