066c12ba47fdaf7ade235852749923695eded7b6
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-21 15:01:09 +0200

    feature(Previous): Previous class implemented

--- src/implement/dijkstra/Previous.java
+++ src/implement/dijkstra/Previous.java
@@ -3,17 +3,21 @@ package implement.dijkstra;
 import interfaces.dijkstra.PreviousInterface;
 import interfaces.graph.VertexInterface;
 
+import java.util.HashMap;
+
 public class Previous implements PreviousInterface
 {
+    private final HashMap<VertexInterface, VertexInterface> viviHashMap = new HashMap<>();
+
     @Override
     public void setFatherOf(VertexInterface object, VertexInterface father)
     {
-
+        this.viviHashMap.put(object, father);
     }
 
     @Override
     public VertexInterface getFatherOf(VertexInterface object)
     {
-        return null;
+        return this.viviHashMap.getOrDefault(object, null);
     }
 }
