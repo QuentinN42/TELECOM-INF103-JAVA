13ebcc477291724e03adb6876d7358de7bd2b13f
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-26 17:24:00 +0100

    docs(Pi): Pi methods javadoc

--- src/implement/dijkstra/Pi.java
+++ src/implement/dijkstra/Pi.java
@@ -13,12 +13,20 @@ public class Pi implements PiInterface
 {
     private final HashMap<VertexInterface, Integer> vertexes = new HashMap<>();
 
+    /**
+     * @param object The object to value
+     * @param value The value for this object
+     */
     @Override
     public void setValueOf(VertexInterface object, int value)
     {
         vertexes.put(object, value);
     }
 
+    /**
+     * @param object The object
+     * @return The value of this object
+     */
     @Override
     public int getValueOf(VertexInterface object)
     {
