2674e358156b1c2579e8829fcf82354876275082
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-26 18:02:11 +0100

    docs(GraphInterface): GraphInterface javadoc

--- src/interfaces/graph/GraphInterface.java
+++ src/interfaces/graph/GraphInterface.java
@@ -2,11 +2,39 @@ package interfaces.graph;
 
 import java.util.Iterator;
 
+/**
+ * Graph interface : a graph of vertexes
+ */
 public interface GraphInterface extends Iterable<VertexInterface>
 {
+    /**
+     * @param element the element to test
+     * @return if the element is in the graph ie a vertex of the graph
+     */
     boolean contains(VertexInterface element);
+
+    /**
+     * @param father the father
+     * @param child the child
+     * @return if father is a father of child
+     */
     boolean isSuccessor(VertexInterface father, VertexInterface child);
+
+    /**
+     * @param one first vertex
+     * @param two second vertex
+     * @return the weight between the first and the second vertex
+     */
     int weight(VertexInterface one, VertexInterface two);
+
+    /**
+     * @return the graph size ie nb of vertexes
+     */
     int size();
+
+    /**
+     * Iterate over the graph
+     * @return Iterator
+     */
     Iterator<VertexInterface> iterator();
 }
