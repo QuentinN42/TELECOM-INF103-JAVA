fb5e3fe1e50b13af180c4c3656540471002273f1
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-26 17:56:28 +0100

    docs(ASetInterface): ASetInterface javadoc

--- src/interfaces/graph/ASetInterface.java
+++ src/interfaces/graph/ASetInterface.java
@@ -2,10 +2,30 @@ package interfaces.graph;
 
 import java.util.Iterator;
 
+/**
+ * Set interface : object that contain objects
+ */
 public interface ASetInterface extends Iterable<VertexInterface>
 {
+    /**
+     * @param element the element to test
+     * @return if element is in set
+     */
     boolean contains(Object element);
+
+    /**
+     * @param element the element to add into the set
+     */
     void add(VertexInterface element);
+
+    /**
+     * @return the set size ie number of items in the set
+     */
     int size();
+
+    /**
+     * Iterate over the set
+     * @return Iterator
+     */
     Iterator<VertexInterface> iterator();
 }
