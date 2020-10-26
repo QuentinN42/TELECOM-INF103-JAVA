e02983fed4d648dbe51d97f78de02e5d5da455d3
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-26 17:57:53 +0100

    docs(VertexInterface): VertexInterface javadoc

--- src/interfaces/graph/VertexInterface.java
+++ src/interfaces/graph/VertexInterface.java
@@ -1,6 +1,13 @@
 package interfaces.graph;
 
+/**
+ * Vertex interface: a vertex in the graph
+ */
 public interface VertexInterface
 {
+    /**
+     * @param element the element to test
+     * @return if this == element
+     */
     boolean equals(Object element);
 }
