b114fc28ebc1cc71c80f1c3f96742bef5972c208
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-26 17:44:24 +0100

    docs(Graph): Graph methods added

--- src/implement/graph/Graph.java
+++ src/implement/graph/Graph.java
@@ -15,24 +15,44 @@ public class Graph implements GraphInterface
     private final ArrayList<VertexInterface> vertexes;
     private final int[][] weights;
 
+    /**
+     * @param vertexes List of all vertexes
+     * @param weights incidence matrix
+     */
     public Graph(List<VertexInterface> vertexes, int[][] weights)
     {
+        // TODO(Quentin Lieumont): Add tests
+        //  Verify list size == w len == w height
         this.vertexes = (ArrayList<VertexInterface>) vertexes;
         this.weights = weights;
     }
 
+    /**
+     * @param element the element to test
+     * @return if the element is in the graph
+     */
     @Override
     public boolean contains(VertexInterface element)
     {
         return vertexes.contains(element);
     }
 
+    /**
+     * @param father first
+     * @param child second
+     * @return if first -> second is a valid edge
+     */
     @Override
     public boolean isSuccessor(VertexInterface father, VertexInterface child)
     {
         return this.weight(father, child) != Integer.MAX_VALUE;
     }
 
+    /**
+     * @param one first
+     * @param two second
+     * @return the value of first -> second if both are in the graph else +inf
+     */
     @Override
     public int weight(VertexInterface one, VertexInterface two)
     {
@@ -46,12 +66,19 @@ public class Graph implements GraphInterface
         }
     }
 
+    /**
+     * @return the graph size ie number of vertexes
+     */
     @Override
     public int size()
     {
         return this.vertexes.size();
     }
 
+    /**
+     * Iterate over the graph
+     * @return Iterator
+     */
     @Override
     public Iterator<VertexInterface> iterator()
     {
