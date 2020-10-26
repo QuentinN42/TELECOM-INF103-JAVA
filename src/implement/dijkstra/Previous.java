6d994a28cfdc7efaed824427d334f5cfbcb8d409
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-10-26 17:27:47 +0100

    docs(Previous): Previous methods javadoc

--- src/implement/dijkstra/Previous.java
+++ src/implement/dijkstra/Previous.java
@@ -5,16 +5,27 @@ import interfaces.graph.VertexInterface;
 
 import java.util.HashMap;
 
+/**
+ * Implement PreviousInterface using HashMap
+ */
 public class Previous implements PreviousInterface
 {
     private final HashMap<VertexInterface, VertexInterface> viviHashMap = new HashMap<>();
 
+    /**
+     * @param object The object to add father
+     * @param father The father of object
+     */
     @Override
     public void setFatherOf(VertexInterface object, VertexInterface father)
     {
         this.viviHashMap.put(object, father);
     }
 
+    /**
+     * @param object The object to get father
+     * @return The father of object or null
+     */
     @Override
     public VertexInterface getFatherOf(VertexInterface object)
     {
