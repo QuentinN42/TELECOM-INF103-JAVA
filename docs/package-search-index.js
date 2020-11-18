1307656d2edd8fd4a7cb3869263938f622bdda31
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-11-18 14:00:39 +0100

    docs(docs): docs added

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"implement.dijkstra"},{"l":"implement.graph"},{"l":"interfaces.dijkstra"},{"l":"interfaces.graph"},{"l":"mymain"},{"l":"test"}]
