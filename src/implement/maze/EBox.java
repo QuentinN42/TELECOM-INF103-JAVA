7b0932a2a6a4a36a1719537f8a63e28b11dd5e6b
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-11-18 14:57:14 +0100

    feature(EBox): EBox added

---
+++ src/implement/maze/EBox.java
@@ -0,0 +1,14 @@
+package implement.maze;
+
+public class EBox extends Box
+{
+    /**
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     */
+    EBox(int x, int y)
+    {
+        super(x, y);
+        super.setLabel("E");
+    }
+}
