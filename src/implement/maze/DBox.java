a80a97629e5b457718dd38f901c5268d9f1251e8
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-11-18 14:54:54 +0100

    feature(DBox): DBox added

---
+++ src/implement/maze/DBox.java
@@ -0,0 +1,14 @@
+package implement.maze;
+
+public class DBox extends Box
+{
+    /**
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     */
+    DBox(int x, int y)
+    {
+        super(x, y);
+        super.setLabel("D");
+    }
+}
