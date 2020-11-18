09f0c2916a01ceeff9148cd78dfb26f4da507500
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-11-18 14:43:35 +0100

    docs(implements.maze): Box javadoc

--- src/implement/maze/Box.java
+++ src/implement/maze/Box.java
@@ -2,27 +2,41 @@ package implement.maze;
 
 public class Box implements interfaces.maze.Box
 {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
+    /**
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     */
     Box(int x, int y)
     {
         this.x = x;
         this.y = y;
     }
 
+
+    /**
+     * @return The x coordinate.
+     */
     @Override
     public int getX()
     {
         return this.x;
     }
 
+    /**
+     * @return The y coordinate.
+     */
     @Override
     public int getY()
     {
         return this.y;
     }
 
+    /**
+     * @return The x and the y coordinates.
+     */
     @Override
     public int[] getCoords()
     {
