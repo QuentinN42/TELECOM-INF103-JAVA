15cc3dbb4a4fcba29b27d0507a0a57150ac6ed75
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-11-18 14:57:05 +0100

    feature(ABox): ABox added

---
+++ src/implement/maze/ABox.java
@@ -0,0 +1,14 @@
+package implement.maze;
+
+public class ABox extends Box
+{
+    /**
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     */
+    ABox(int x, int y)
+    {
+        super(x, y);
+        super.setLabel("A");
+    }
+}
