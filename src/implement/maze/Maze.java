838ce3529cdbc15d1751e6c80bc3e90d391ac9ba
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2020-12-05 12:10:03 +0100

    feature(Maze): GraphInterface method signature

--- src/implement/maze/Maze.java
+++ src/implement/maze/Maze.java
@@ -1,6 +1,40 @@
 package implement.maze;
 
+import interfaces.graph.GraphInterface;
+import interfaces.graph.VertexInterface;
+
+import java.util.Iterator;
+
 public class Maze implements GraphInterface
 {
 
+    @Override
+    public boolean contains(VertexInterface element)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isSuccessor(VertexInterface father, VertexInterface child)
+    {
+        return false;
+    }
+
+    @Override
+    public int weight(VertexInterface one, VertexInterface two)
+    {
+        return 0;
+    }
+
+    @Override
+    public int size()
+    {
+        return 0;
+    }
+
+    @Override
+    public Iterator<VertexInterface> iterator()
+    {
+        return null;
+    }
 }
