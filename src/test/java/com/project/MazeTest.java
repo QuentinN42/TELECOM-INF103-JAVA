63ebd855e905c5e8c22423c67f0c73d378283ccf
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2021-01-04 18:26:02 +0100

    test(MazeTest): Starting MazeTest

---
+++ src/test/java/com/project/MazeTest.java
@@ -0,0 +1,119 @@
+package com.project;
+
+import com.project.error.MazeException;
+import com.project.implement.maze.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MazeTest
+{
+    Box box1;
+    Box box2;
+    Box box3;
+    Box box4;
+    Maze maze1;
+
+    @BeforeEach
+    void setUp() throws MazeException
+    {
+        box1 = Box.newBox(0,0, "D");
+        box2 = Box.newBox(0,1, "E");
+        box4 = Box.newBox(1,0, "W");
+        box3 = Box.newBox(1,1, "A");
+
+        maze1 = new Maze(
+            new HashMap<>() {{
+                put(box1.getCoords(), box1);
+                put(box2.getCoords(), box2);
+                put(box3.getCoords(), box3);
+                put(box4.getCoords(), box4);
+            }},
+            2,
+            2
+        );
+    }
+
+    @Test
+    void equals()
+    {
+        assertEquals(maze1, maze1);
+    }
+
+    @Test
+    void initFromTextFile() throws MazeException
+    {
+        Maze maze2 = new Maze("src/test/resources/maze1");
+        assertEquals(maze1, maze2);
+    }
+
+    @Test
+    void saveToTextFile()
+    {
+    }
+
+    @Test
+    void contains()
+    {
+    }
+
+    @Test
+    void isSuccessor()
+    {
+    }
+
+    @Test
+    void weight()
+    {
+    }
+
+    @Test
+    void size()
+    {
+    }
+
+    @Test
+    void iterator()
+    {
+    }
+
+    @Test
+    void get()
+    {
+    }
+
+    @Test
+    void testGet()
+    {
+    }
+
+    @Test
+    void getByLabel()
+    {
+    }
+
+    @Test
+    void path()
+    {
+    }
+
+    @Test
+    void testPath()
+    {
+    }
+
+    @Test
+    void testPath1()
+    {
+    }
+
+    @Test
+    void testPath2()
+    {
+    }
+}
