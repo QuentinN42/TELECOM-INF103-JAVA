782a0043eb200c59fd6a7beecf7cf65e8d44df81
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2021-01-05 23:01:59 +0100

    test(MazeTest): tests done

--- src/test/java/com/project/MazeTest.java
+++ src/test/java/com/project/MazeTest.java
@@ -3,13 +3,15 @@ package com.project;
 import com.project.error.MazeException;
 import com.project.implement.maze.box.Box;
 import com.project.implement.maze.Maze;
+import com.project.interfaces.graph.VertexInterface;
 import com.project.io.File;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MazeTest
 {
@@ -17,6 +19,7 @@ class MazeTest
     Box box2;
     Box box3;
     Box box4;
+    List<Box> boxes;
     Maze maze1;
 
     @BeforeEach
@@ -26,6 +29,8 @@ class MazeTest
         box2 = Box.newBox(0,1, "E");
         box4 = Box.newBox(1,0, "W");
         box3 = Box.newBox(1,1, "A");
+
+        boxes = Arrays.asList(box1, box2, box3, box4);
 
         maze1 = new Maze(
             new HashMap<>() {{
@@ -70,62 +75,78 @@ class MazeTest
     }
 
     @Test
-    void contains()
+    void contains() throws MazeException
     {
+        for (Box box: boxes)
+            assertTrue(maze1.contains(box));
+
+        assertTrue(maze1.contains(Box.newBox(1,1, "A")));
+
+        assertFalse(maze1.contains(Box.newBox(1,1, "W")));
     }
 
     @Test
     void isSuccessor()
     {
+        assertTrue(maze1.isSuccessor(box1,box2));
+        assertTrue(maze1.isSuccessor(box2,box3));
+
+        assertFalse(maze1.isSuccessor(box1, box4));
+        assertFalse(maze1.isSuccessor(box3, box4));
     }
 
     @Test
     void weight()
     {
+        assertEquals(1, maze1.weight(box1,box2));
+        assertEquals(1, maze1.weight(box2,box3));
+
+        assertEquals(Integer.MAX_VALUE, maze1.weight(box1, box4));
+        assertEquals(Integer.MAX_VALUE, maze1.weight(box3, box4));
     }
 
     @Test
     void size()
     {
+        assertEquals(4, maze1.size());
     }
 
     @Test
     void iterator()
     {
+        Set<Box> test = new HashSet<>();
+        for (VertexInterface box: maze1)
+            test.add((Box) box);
+        assertEquals(new HashSet<>(boxes), test);
     }
 
     @Test
     void get()
     {
-    }
-
-    @Test
-    void testGet()
-    {
+        for (Box box: boxes)
+        {
+            assertEquals(box, maze1.get(box.getCoords()));
+            assertEquals(box, maze1.get(box.getY(), box.getX()));
+        }
     }
 
     @Test
     void getByLabel()
     {
+        for (Box box: boxes)
+        {
+            assertEquals(box, maze1.getByLabel(box.getLabel()));
+        }
+
     }
 
     @Test
     void path()
     {
-    }
-
-    @Test
-    void testPath()
-    {
-    }
-
-    @Test
-    void testPath1()
-    {
-    }
-
-    @Test
-    void testPath2()
-    {
+        List<Box> path = Arrays.asList(box1, box2, box3);
+        assertEquals(path, maze1.path(box1, box3));
+        assertEquals(path, maze1.path("D", box3));
+        assertEquals(path, maze1.path(box1, "A"));
+        assertEquals(path, maze1.path("D", "A"));
     }
 }
