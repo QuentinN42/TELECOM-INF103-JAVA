bd2d793456711e32fd5c4232c13a2a1f8d7dd18f
Quentin Lieumont <quentin.lieumont@telecom-paris.fr>
2021-01-18 19:09:42 +0100

    feature(GUI): Draw solution

--- src/main/java/com/project/gui/GUI.java
+++ src/main/java/com/project/gui/GUI.java
@@ -4,15 +4,13 @@ package com.project.gui;
 import com.project.error.MazeException;
 import com.project.implement.maze.Maze;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.Serial;
 import java.util.logging.Logger;
-import javax.swing.*;
-
-import static java.lang.String.format;
 
 
 public class GUI extends JFrame implements ActionListener
@@ -24,6 +22,7 @@ public class GUI extends JFrame implements ActionListener
     public static final int SAMPLE_CANVAS_WIDTH  = 500;
     public static final int SAMPLE_CANVAS_HEIGHT = 500;
     final JFileChooser fc = new JFileChooser();
+    private boolean solve;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(GUI::new);
@@ -38,7 +37,7 @@ public class GUI extends JFrame implements ActionListener
         this.makeMenu();
 
         maze = null;
-        mazeGUI = new MazeGUI(maze);
+        mazeGUI = new MazeGUI(null, false);
 
         this.drawMaze();
 
@@ -55,6 +54,7 @@ public class GUI extends JFrame implements ActionListener
         Menu file = new Menu("File");
         mBar.add(file);
         file.add(new MenuItem("Open"));
+        file.add(new MenuItem("Solve"));
         file.addActionListener(this);
 
         Menu exit = new Menu("Exit");
@@ -79,31 +79,50 @@ public class GUI extends JFrame implements ActionListener
             logger.info(() -> "Quit.");
             System.exit(0);
         }
-        if (e.getActionCommand().equals("Open")) {
-            int returnVal = fc.showOpenDialog(this);
-
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File file = fc.getSelectedFile();
-
-                logger.info(() -> "Opening: " + file.getAbsolutePath() + ".");
-                this.initMaze(file.getAbsolutePath());
-            } else {
-                logger.info(() -> "Open command cancelled by user.");
-            }
+        if (e.getActionCommand().equals("Open"))
+        {
+            logger.info(() -> "Open file.");
+            if(this.openFile())
+                this.solve = false;
         }
-        this.mazeGUI = new MazeGUI(maze);
+        if (e.getActionCommand().equals("Solve"))
+        {
+            logger.info(() -> "Solving.");
+            this.solve = true;
+        }
+        this.mazeGUI = new MazeGUI(maze, solve);
         this.drawMaze();
     }
 
-    private void initMaze(String absolutePath)
+    private boolean openFile()
+    {
+        int returnVal = fc.showOpenDialog(this);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION)
+        {
+            File file = fc.getSelectedFile();
+
+            logger.info(() -> "Opening: " + file.getAbsolutePath() + ".");
+            return this.initMaze(file.getAbsolutePath());
+        }
+        else
+        {
+            logger.info(() -> "Open command cancelled by user.");
+            return false;
+        }
+    }
+
+    private boolean initMaze(String absolutePath)
     {
         try(Maze tmp = new Maze(absolutePath))
         {
             this.maze = tmp;
+            return true;
         }
         catch (MazeException ex)
         {
             logger.throwing("GUI", "initMaze", ex);
+            return false;
         }
     }
 }
--- src/main/java/com/project/gui/MazeGUI.java
+++ src/main/java/com/project/gui/MazeGUI.java
@@ -12,11 +12,13 @@ import java.io.Serial;
 
 class MazeGUI extends JPanel
 {
+    private final boolean needSolve;
     Maze maze;
 
-    public MazeGUI(Maze maze)
+    public MazeGUI(Maze maze, boolean needSolve)
     {
         this.maze = maze;
+        this.needSolve = needSolve;
     }
 
     @Serial
@@ -33,16 +35,31 @@ class MazeGUI extends JPanel
     private void initGraphic(Graphics graphics)
     {
         super.paintComponent(graphics);
-        setBackground(Color.BLACK);
-        graphics.setColor(Color.WHITE);
+        setBackground(Color.GRAY);
+        graphics.setColor(Color.BLACK);
         graphics.setFont(new Font("Monospaced", Font.PLAIN, 12));
     }
 
     private void drawMaze(Graphics graphics)
     {
+        graphics.setColor(Color.BLACK);
         for (VertexInterface vi: maze)
         {
             Box box = ((Box) vi);
+            int x = box.getX();
+            int y = box.getY();
+            graphics.drawRect(x*25, y*25, 25, 25);
+            graphics.drawString(box.getLabel(), x*25+10, y*25+15);
+        }
+        if(needSolve)
+            this.addSolution(graphics);
+    }
+
+    private void addSolution(Graphics graphics)
+    {
+        graphics.setColor(Color.RED);
+        for (Box box: maze.path("D", "A"))
+        {
             int x = box.getX();
             int y = box.getY();
             graphics.drawRect(x*25, y*25, 25, 25);
